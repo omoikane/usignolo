77b75acbb97d516e7de9b6dc5f34e77225198f92
Daniele <danieledenobili@gmail.com>
2013-10-09 00:30:51 +0200

    My first bandle!

---
+++ src/Usignolo/Bundle/UsignoloBundle/UsignoloBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of Usignolo.
+ *
+ * (c) Daniele De Nobili <danieledenobili@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Usignolo\Bundle\UsignoloBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UsignoloBundle extends Bundle
+{
+}
