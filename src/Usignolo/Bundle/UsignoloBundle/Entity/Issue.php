6b9c5ab654a4020cb6231225d1dd2fc2a8747ecc
Daniele <danieledenobili@gmail.com>
2013-10-11 23:10:59 +0200

    An issue tracker should have an issue entity

---
+++ src/Usignolo/Bundle/UsignoloBundle/Entity/Issue.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Usignolo\Bundle\UsignoloBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Issue
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Usignolo\Bundle\UsignoloBundle\Entity\IssueRepository")
+ */
+class Issue
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Issue
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Issue
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
---
+++ src/Usignolo/Bundle/UsignoloBundle/Tests/Entity/IssueTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of Usignolo.
+ *
+ * (c) Daniele De Nobili <danieledenobili@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Usignolo\Bundle\UsignoloBundle\Tests\Entity;
+
+use Usignolo\Bundle\UsignoloBundle\Entity\Issue;
+
+class IssueTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSetTitle()
+    {
+        $issue = new Issue();
+
+        $issue->setTitle('There is an issue!');
+
+        $this->assertEquals('There is an issue!', $issue->getTitle());
+    }
+
+    public function testGetSetDescription()
+    {
+        $issue = new Issue();
+
+        $issue->setDescription('<p>And there is a description too.</p>');
+
+        $this->assertEquals('<p>And there is a description too.</p>', $issue->getDescription());
+    }
+}
