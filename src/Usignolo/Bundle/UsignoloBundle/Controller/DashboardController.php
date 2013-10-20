12c0fec60d64737fbf074ea18423bb23fe629732
Daniele <danieledenobili@gmail.com>
2013-10-20 22:07:34 +0200

    [Insight] Unused use statement should be avoided

--- src/Usignolo/Bundle/UsignoloBundle/Controller/DashboardController.php
+++ src/Usignolo/Bundle/UsignoloBundle/Controller/DashboardController.php
@@ -14,7 +14,6 @@ namespace Usignolo\Bundle\UsignoloBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Home controller.
